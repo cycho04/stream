a83b43e92b9ba1b263f442909f28777d5b461df2
Chris Y Cho <chrisycho04@gmail.com>
2019-02-01 07:46:46 -0800

    added lodash for omit and mapKeys. RESTful reducer is finished and working

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 import { reducer as formReducer} from 'redux-form';
 
 export default combineReducers({
     auth: authReducer,
-    form: formReducer //the key needs to be exactly 'form' for redux-form
+    form: formReducer, //the key needs to be exactly 'form' for redux-form
+    streams: streamReducer
 })
